6111b2f536da26d1617efbd676ed413a889cd62e
Арсений <argo24.11.05@gmail.com>
2022-03-03 17:23:11 +0300

    Изменена форма для регистрации

--- loginform.py
+++ loginform.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, RadioField, SubmitField
 from wtforms.validators import DataRequired, Email
 
 
 class LoginForm(FlaskForm):
     username = StringField('ФИО', validators=[DataRequired()])
-    '''email = StringField('Почта', validators=[DataRequired(), Email()])
+    email = StringField('Почта', validators=[DataRequired(), Email()])
     login = StringField('Логин', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     password2 = PasswordField('Подтверждение пароля', validators=[DataRequired()])
-    submit = SubmitField('Войти')'''
+    client_type = RadioField("Войти как:", choices=[("client", "Клиент"), ("manager", "Менеджер")])
+    submit = SubmitField('Войти')
